307c829dd817cbc653f799c5a2b77cd30b753be2
chayapatr <chayapatr.arc@gmail.com>
2019-03-05 23:17:27 +0700

    🐛 minor commit

---
+++ date/5/bulb.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main(){
+    // unsigned int bulb = 0;
+    // int num;
+    // cin >> num;
+    // for()
+    // for(int i=0;i<num;i++) cin >> a[i];
+    const int n = 6;
+    bitset<n> bulb("000001");
+    cout << bulb;
+}
